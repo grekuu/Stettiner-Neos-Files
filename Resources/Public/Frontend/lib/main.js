ddfb522c707a82a9bc1da89ccc80713bbd63faa0
grekuu <gracjanprusik@gmail.com>
2022-12-17 13:31:17 +0100

    project finished

--- Resources/Public/Frontend/lib/main.js
+++ Resources/Public/Frontend/lib/main.js
@@ -1,5 +1,10 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('Hello Bulma!');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const hamburgerItems = document.getElementById("hamburgerItems");
+
+  hamburger.addEventListener("click", function onClick() {
+    hamburgerItems.classList.toggle("mobile-href");
+  });
 });
